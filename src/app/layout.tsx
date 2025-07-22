f164fc180ba957850f5053f166e2fbf74f9116d1
lokrim <ahmedzyed123@gmail.com>
2025-07-22 19:46:27 +0530

    added pokemon details page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import ThemeButton from "@/components/theme-button";
-import { Providers } from "./providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
